89cd0137fb03ae63e26aac31b33d6f9902ba8e74
yuvi9599 <131909249+yuvi9599@users.noreply.github.com>
2024-09-20 16:40:09 +0530

    Add files via upload

---
+++ simon game/simon.js
@@ -0,0 +1,95 @@
+let gameseq=[];
+ let userseq=[];
+
+ let btns=["yellow","red","green","purple"];
+
+let started=false;
+let level=0;
+ let h2=document.querySelector("h2");
+
+document.addEventListener("keypress",function(){
+    if(started==false){
+        console.log("game is started");
+        started=true;
+        levelup();
+
+    }
+});
+function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250);
+    }
+    function userflash(btn){
+        btn.classList.add("userflash");
+        setTimeout(function(){
+            btn.classList.remove("userflash");
+        },250);
+
+    }
+
+
+
+function levelup(){
+    userseq=[];
+    level++;
+    h2.innerText=`Level ${level}`;
+    //random btn choose
+    let randIdx=Math.floor(Math.random()*3);
+    let randColor=btns[randIdx];
+    //console.log(randIdx);
+    //console.log(randColor);
+    let randbtn=document.querySelector(`.${randColor}`);
+    gameseq.push(randColor);
+    console.log(gameseq);
+ 
+     gameFlash(randbtn);
+
+}
+function checkAns(idx){
+    
+    
+    if(userseq[idx]==gameseq[idx]){
+        if(userseq.length==gameseq.length){
+            setTimeout(levelup,1000);
+            
+        }
+        
+    }else{
+        h2.innerHTML=` Game Over ! Your score was <b>${level}</b> <br>Press any key to start`; 
+        
+        document.querySelector("body").style.backgroundColor="red";
+        setTimeout(function(){
+            document.querySelector("body").style.color="white";
+
+
+        },150);
+        reset();
+
+    }
+}
+function btnPress(){
+    //console.log(this);
+   let btn=this;
+   userflash(btn);
+    userColor=btn.getAttribute("id");
+    userseq.push(userColor);
+
+    checkAns(userseq.length-1);
+
+}
+let allBtns=document.querySelectorAll(".btn");
+for( btn of allBtns){
+    btn.addEventListener("click",btnPress);
+
+}
+function reset(){
+    started=false;
+    gameseq=[];
+    userseq=[];
+    level=0;
+}
+
+
+
